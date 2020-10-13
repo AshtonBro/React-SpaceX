64951eff121950ae3ee1e3920ba011946e1c2664
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-10-13 23:41:21 +0500

    add videos, connect videos, create Header / Main, add styles for components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+
 import './style.css';
 
 function App() {
   return (
-    <header className="header">
-		<img
-				src={logo}
-				alt="Logo Space X"
-				className="logo"
-		/>
-		<nav className="main-nav nav">
-			<ul className="list">
-				<li className="item">
-					<a href="#" className="item-link">Falcon 1</a>
-				</li>
-				<li className="item">
-					<a href="#" className="item-link">Falcon 9</a>
-				</li>
-				<li className="item">
-					<a href="#" className="item-link">Falcon Heavy</a>
-				</li>
-				<li className="item">
-					<a href="#" className="item-link">Updates</a>
-				</li>
-			</ul>
-		</nav>
-		<nav className="secondary-nav">
-			<ul className="list">
-				<li className="item">
-					<a href="#" className="item-link">Home</a>
-				</li>
-				<li className="item">
-					<a href="calendar.html" className="item-link">Calendar</a>
-				</li>
-			</ul>
-		</nav>
-	</header>
+    <>
+      <Header />
+      <Main />
+    </>
   )
 }
 
