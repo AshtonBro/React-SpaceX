18e63fdb59d82e1b871a640cc20aebc5343d4854
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-10-14 20:46:12 +0500

    create Features components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import Features from './components/Features/Features';
 
 import './style.css';
 
@@ -10,6 +10,7 @@ function App() {
     <>
       <Header />
       <Main />
+      <Features />
     </>
   )
 }
