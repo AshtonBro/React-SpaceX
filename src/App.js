e5751d6d648c771934b4baf2deb9f724e745fb7a
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-10-14 21:36:15 +0500

    create Details component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Features from './components/Features/Features';
 import Footer from './components/Footer/Footer';
-import Calendar from './components/Calendar/Calendar';
 
 import './style.css';
 
@@ -13,7 +12,6 @@ function App() {
       <Header />
       <Main />
       <Features />
-      <Calendar />
       <Footer />
     </>
   )
