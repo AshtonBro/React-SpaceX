8057414daccbb4d20d63acbb40b44476ed14936b
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-10-14 23:05:28 +0500

    desctructur and props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
 
 import { Calendar, Details, Features, Footer, Header, Main } from './components';
-
 import './style.css';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Main />
-      <Features />
-      <Footer />
-    </>
-  )
+class App extends React.Component {
+
+  state = {
+    rocket: 'Falcon-1',
+  };
+
+  method() {
+
+  };
+
+  render() {
+    return(
+      <>
+        <Header />
+        <Main rocket={this.state.rocket}/>
+        <Features />
+        <Footer />
+      </>
+    )
+  }
 }
 
 export default App;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,18 @@
 import React from "react";
 import './main.css';
 
-const Main = () => (
-    <section className="main">
-        <h1 className="title">
-        Falcon 1
-        </h1>
+const Main = ({ rocket }) => {
+    return (
+        <section className="main">
+            <h1 className="title">
+                {rocket}
+            </h1>
 
-        <div className="video-container">
-            <video className="video" autoPlay loop muted src="./video/moon.mp4"></video>
-        </div>
-    </section>
-);
+            <div className="video-container">
+                <video className="video" autoPlay loop muted src="./video/moon.mp4"></video>
+            </div>
+        </section>
+    )
+};
 
 export default Main;
