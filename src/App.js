d58d0e39ec494294cd45f1f907d60ee59072ab24
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-10-14 23:55:44 +0500

    updateRocket, find, map.rockets and drop to navTitle

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React from 'react';
-
 import { Calendar, Details, Features, Footer, Header, Main } from './components';
+import FetchData from './service/FetchData';
 import './style.css';
 
 class App extends React.Component {
 
+  fetchData = new FetchData();
+
   state = {
     rocket: 'Falcon-1',
+    rocketFeatures: null,
+    rockets: [],
   };
 
-  method() {
-
+  componentDidMount() {
+    this.updateRocket();
   };
+
+  updateRocket() {
+    this.fetchData.getRocket()
+    .then((data) => {
+      this.setState({ rockets: data.map(item => item.name) })
+      return data
+    })
+    .then((data) => data.find((item) => item.name === this.state.rocket))
+    .then((rocketFeatures) => {
+      this.setState({ rocketFeatures }, () => console.log(this.state.rocket));
+    });
+  }
 
   render() {
     return(
       <>
-        <Header />
+        <Header rockets={this.state.rockets}/>
         <Main rocket={this.state.rocket}/>
         <Features />
         <Footer />
