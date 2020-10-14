806ba445c688bac1b39815e8cfa1be8d6d6e02db
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-10-14 20:50:27 +0500

    create Footer js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Features from './components/Features/Features';
+import Footer from './components/Footer/Footer';
 
 import './style.css';
 
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Main />
       <Features />
+      <Footer />
     </>
   )
 }
