ddf334c458f575afe37bb8c870009735b307873f
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-10-16 12:22:44 +0500

    restructurization links, added summary description

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,39 +2,35 @@ import React from 'react';
 import logo from '../../logo.svg';
 import './footer.css';
 
-
-const Footer = ({ elon_twitter: elonMask, flickr, twitter, website }) => (
-<footer className="footer">
+const Footer = ({links , summary }) => (
+    <footer className="footer">
         <img src={logo} alt="logo Space X" className="logo"/>
             <nav className="footer-nav">
                 <ul className="list">
                     <li className="item">
-                        <a href={elonMask}
+                        <a href={links.elon_twitter}
                         rel="noopener noreferrer" 
                         target='_blank' 
                         className="item-link">Elon Musk Twitter</a></li>
                     <li className="item">
-                        <a href={twitter}
+                        <a href={links.twitter}
                         rel="noopener noreferrer" 
                         target='_blank' 
                         className="item-link">Twitter</a></li>
                     <li className="item">
-                        <a href={flickr}
+                        <a href={links.flickr}
                         rel="noopener noreferrer" 
                         target='_blank' 
                         className="item-link">Flickr</a></li>
                     <li className="item">
-                        <a href={website}
+                        <a href={links.website}
                         rel="noopener noreferrer" 
                         target='_blank' 
                         className="item-link">Website</a></li>
                 </ul>
             </nav>
             <p className="footer-text">
-                For additional questions, contact
-                <a className="footer-link" href="mailto:rideshare@spacex.com"
-                >rideshare@spacex.com</a
-                >
+                {summary}
             </p>
     </footer>
 );
