fa77392a2e1655eaa31d9a80e072569e051e42e0
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-10-20 00:11:25 +0500

    Preloader, added YouTube framework, descr = discription

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,36 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Main from '../Main/Main.js'
-
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import useLaunches from '../useLaunches/useLaunches';
+import Main from '../Main/Main.js';
+import YouTube from 'react-youtube';
 import './details.css';
 
-const Details = () => (
+const Details = (props) => {
+
+    const [launch, setLaunch] = useState({});
+    const { getLaunch } = useLaunches();
+
+    useEffect(() => {
+       setLaunch(getLaunch(props.match.params.id))
+    }, [getLaunch]);
+
+    
+    const history = useHistory();
+    
+    //? TODO: Прелоадер
+    if(!launch) return <div>Загрузка...</div> 
+  
+    return (
     <>
-    <Main />
-    <main className="details">
-        <div className="container">
-            <div className="details-row">
-                <div className="details-image">
-                    <img src="https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png" alt=""/>
+        <Main name={launch.name}/>
+        <main className="details">
+            <div className="container">
+                <div className="details-row">
+                    <div className="details-image">
+                        <img src={launch.links?.patch.small} alt={launch.name}/>
+                    </div>
+                    <div className="details-content">
+                    <p className="details-description">{launch?.details}</p>
+                    </div>
                 </div>
-                <div className="details-content">
-                    <p className="details-description">Engine failure at 33 seconds and loss of vehicle</p>
-                </div>
+                <YouTube className="details-youtube" videoId={launch.links?.youtube_id} />
             </div>
-            <div>
-                <iframe className="details-youtube"
-                width="560" 
-                height="315" 
-                src="https://www.youtube.com/embed/dLQ2tZEH6G0" 
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                allowFullScreen
-                />
-            </div>
-        </div>
-        <Link to="/calendar" className="button button-back">go back</Link>
-    </main>
+            <a onClick={history.goBack} className="button button-back">go back</a>
+        </main>
     </>
-)
+    )
+}
 
 export default Details;
